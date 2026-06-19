8cb60525e27f1c8861ee91e89168acdd81e3797b
Dwerkjem <derekrneilson@gmail.com>
2026-06-19 15:14:26 -0600

    added starter code

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 def main():
     total_attendees = 0
-    while True:
-        print(total_attendees)
-        return
+    run = True
+    while run:
+        company_name = input("What is your companies name: ")
+        while True:
+            try:
+                attendees = int(input(f"How many are attending from {company_name}: "))
+                if attendees + total_attendees > 125:
+                    raise OverflowError(
+                        f"There are not enough space available {125 - total_attendees} seats are left please select less than that or enter zero to quit"
+                    )
+                if attendees == 0:
+                    run = False
+                total_attendees += attendees
+                break
+            except ValueError:
+                print("Please type a whole number ie.(45)")
+            except OverflowError as err:
+                print(err)
 
 
 if __name__ == "__main__":
